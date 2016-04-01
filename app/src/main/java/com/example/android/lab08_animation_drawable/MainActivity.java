12987035924d7af99b138cd9c710db1d18ceda22
smalljay1117 <smalljay1117>
2016-04-01 12:16:13 +0800

    Runnable

---
+++ app/src/main/java/com/example/android/lab08_animation_drawable/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.android.lab08_animation_drawable;
+
+import android.content.res.TypedArray;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView m_img_duke;
+    private AnimationDrawable m_frame_animation;
+    private TextView m_tv_message;
+
+    private View m_view_logo;
+    private TextView m_logo_name;
+    private TextView m_view_message;
+    private Button m_btn_go;
+
+    private TypedArray mNbaLogos;
+    private int mNbaLogosCount;
+//    private String[] mNbaLogoNames;
+//    private int mNbaLogoNamesCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initFrameAnimation();
+        initNbaLogos();
+    }
+
+    private void initNbaLogos() {
+        mNbaLogos = getNbaLogos();
+        mNbaLogosCount = getNbaLogos().length();
+        m_view_logo.setBackground(mNbaLogos.getDrawable(0));
+//        mNbaLogoNames = getNbaLogoNames();
+//        mNbaLogoNamesCount = getNbaLogoNames().toString().length();
+
+    }
+
+    private TypedArray getNbaLogos() {
+        TypedArray logos = getResources().obtainTypedArray(R.array.nba_logos);
+        return logos;
+    }
+
+//    private String[] getNbaLogoNames() {
+//        String[] logonames = getResources().getStringArray(R.array.nba_logo_name);
+//        return logonames;
+//    }
+
+    private void initView() {
+        m_img_duke = (ImageView)findViewById(R.id.img_duke);
+        m_tv_message = (TextView)findViewById(R.id.tv_message);
+
+        m_view_logo = findViewById(R.id.view_logo);
+        m_logo_name = (TextView)findViewById(R.id.tv_logo_name);
+        m_view_message = (TextView)findViewById(R.id.view_message);
+
+        m_btn_go = (Button)findViewById(R.id.btn_go);
+    }
+
+    private void initFrameAnimation() {
+        m_img_duke.setBackgroundResource(R.drawable.frame_animation);
+        m_frame_animation = (AnimationDrawable)m_img_duke.getBackground();
+    }
+
+    public void click(View view) {
+        switch (view.getId()) {
+            case R.id.btn_start:
+                m_frame_animation.start();
+                break;
+            case R.id.btn_stop:
+                m_frame_animation.stop();
+                break;
+            case R.id.btn_5_secs:
+                animation5secs();
+                break;
+        }
+    }
+
+    private Handler m_Handler = new Handler();
+
+    private void animation5secs() {
+        int delayMillis = 5 * 1000;
+        Runnable task = new Task();
+        boolean result = m_Handler.postDelayed(task, delayMillis);
+
+        m_tv_message.setText(result ? "交付成功" : "交付失敗");
+        m_frame_animation.start();
+    }
+
+    public void go(View view) {
+        m_Handler.post(mStarRandomTask);
+        m_Handler.postDelayed(mStopRandomTask, 3000);
+        m_btn_go.setEnabled(false);
+        m_btn_go.setText("選擇中...");
+    }
+
+    private StartRandomTask mStarRandomTask = new StartRandomTask();
+    private StopRandomTask mStopRandomTask = new StopRandomTask();
+
+    private class StartRandomTask implements Runnable {
+        @Override
+        public void run() {
+            int index = (int)(Math.random() * mNbaLogosCount);
+            m_view_logo.setBackground(mNbaLogos.getDrawable(index));
+            m_Handler.postDelayed(this, 1);
+        }
+    }
+
+    private class StopRandomTask implements Runnable {
+        @Override
+        public void run() {
+            m_Handler.removeCallbacks(mStarRandomTask);
+            m_btn_go.setEnabled(true);
+            m_btn_go.setText("GO");
+        }
+    }
+
+    private class Task implements Runnable {
+        @Override
+        public void run() {
+            m_frame_animation.stop();
+            m_tv_message.setText("時間到");
+        }
+    }
+}
